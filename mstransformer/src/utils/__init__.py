7edede76a832fee766ce7b9db03fdcfab92ea499
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-02-09 20:37:57 -0800

    added test files for dataset

---
+++ mstransformer/src/utils/__init__.py
@@ -0,0 +1,4 @@
+# Package for utility files including:
+#   - dataset: Load MUSDB18 data.
+
+from mstransformer.src.utils.dataset import MUSDBDataset, load_dataset
--- mstransformer/test/utils/test_dataset.py
+++ mstransformer/test/utils/test_dataset.py
@@ -1,4 +1,13 @@
+from mstransformer.src.utils import MUSDBDataset, load_dataset
 
 
-def test_load_info():
-    assert 3 == 3
+def test_musdb():
+    # NOTE: First test run will take roughly 10 minutes to load the dataset.
+    # Subsequent tests should run in < 45 secs.
+    musdb = MUSDBDataset(download=True, samples_per_track=1, duration=1.0)
+    for x, y in musdb:
+        assert x.shape[-1] == 44100
+
+
+def test_load_dataset():
+    _, _ = load_dataset(download=True)
