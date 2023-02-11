75becd174c3b2a7c09085d76c00f0b89ac382c8a
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-02-10 16:21:39 -0800

    Implemented transformer Encoder by putting EncoderBlocks in sequence.

--- mstransformer/src/transformer/__init__.py
+++ mstransformer/src/transformer/__init__.py
@@ -1,3 +1,4 @@
 # Package for MSTransformer base model architecture.
 
+from mstransformer.src.transformer.encoder import Encoder, EncoderBlock
 from mstransformer.src.transformer.position import PositionalEncoding
--- mstransformer/src/transformer/encoder.py
+++ mstransformer/src/transformer/encoder.py
@@ -1,22 +1,29 @@
-import torch
 from torch import nn
 
 from mstransformer.src.transformer.position import PositionalEncoding
 
 
 class EncoderBlock(nn.Module):
+    """
+    A single encoder block for the transformer encoder. Includes a
+    MultiHeadAttention layer followed by a FeedForward layer.
+
+    Parameters:
+        - d_model (int): dimension of input.
+        - num_heads (int): number of attention heads.
+        - dropout (float): dropout probability.
+    """
     def __init__(self, d_model, num_heads=8, dropout=0.0):
         super(EncoderBlock, self).__init__()
-
         self.attention = nn.MultiheadAttention(embed_dim=d_model,
                                                num_heads=num_heads,
                                                dropout=dropout)
         self.norm1 = nn.LayerNorm(normalized_shape=d_model)
         self.dropout1 = nn.Dropout(p=dropout)
         self.feedforward = nn.Sequential(
-            nn.Linear(d_model, d_model * 4),
+            nn.Linear(d_model, d_model * 8),
             nn.ReLU(),
-            nn.Linear(d_model * 4, d_model)
+            nn.Linear(d_model * 8, d_model)
         )
         self.norm2 = nn.LayerNorm(normalized_shape=d_model)
         self.dropout2 = nn.Dropout(p=dropout)
@@ -28,16 +35,25 @@ class EncoderBlock(nn.Module):
 
 
 class Encoder(nn.Module):
+    """
+    Lays separate EncoderBlocks in sequence to create the encoder.
+
+    Parameters:
+        - num_layers (int): number of encoder blocks.
+        - num_channels (int): dimension of input.
+        - num_heads (int): number of attention heads.
+        - max_len (int): length of the input sequence.
+        - dropout (float): dropout probability.
+    """
     def __init__(
             self,
-            num_channels,
+            num_layers=6,
+            num_channels=2,
             num_heads=8,
-            dropout=0.0,
-            max_len=44100*5,
-            num_layers=6
+            max_len=220500,
+            dropout=0.0
     ):
         super(Encoder, self).__init__()
-
         self.positional_encoding = PositionalEncoding(max_len, num_channels)
         self.blocks = [
             EncoderBlock(num_channels, num_heads, dropout)
@@ -45,7 +61,7 @@ class Encoder(nn.Module):
         ]
 
     def forward(self, x):
-        seq_len, d_model = x.size(1), x.size(2)
+        x_enc = self.positional_encoding(x)
         for block in self.blocks:
-            pass
-        pass
+            x_enc = block(x_enc)
+        return x_enc
