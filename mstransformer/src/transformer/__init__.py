77c364f1515194b428cb55c2d4dfef8d8c3e5cee
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-02-11 22:09:46 -0800

    added PostprocessLayer to mstransformer package.

--- mstransformer/src/transformer/__init__.py
+++ mstransformer/src/transformer/__init__.py
@@ -4,4 +4,4 @@ from mstransformer.src.transformer.decoder import Decoder, DecoderBlock
 from mstransformer.src.transformer.encoder import Encoder, EncoderBlock
 from mstransformer.src.transformer.mstransformer import MSTransformer
 from mstransformer.src.transformer.position import PositionalEncoding
-from mstransformer.src.transformer.preprocess import PreprocessLayer
+from mstransformer.src.transformer.preprocess import PreprocessLayer, PostprocessLayer
