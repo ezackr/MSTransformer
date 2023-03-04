d076b26064be821e2a2bf0914251cbf95d10582e
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-03-04 12:59:28 -0800

    implemented base DownSample and UpSample layers.

--- mstransformer/src/transformer/sampling.py
+++ mstransformer/src/transformer/sampling.py
@@ -20,14 +20,28 @@ class ConvLayer(nn.Module):
 class DownSample(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(DownSample, self).__init__()
+        self.conv = ConvLayer(in_channels, out_channels)
+        self.max_pool = nn.MaxPool1d(kernel_size=2)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x_conv = self.conv(x)
+        x_pool = self.max_pool(x_conv)
+        return x_conv, x_pool
 
 
 class UpSample(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UpSample, self).__init__()
+        self.conv_transpose = nn.ConvTranspose1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=(2,),
+            stride=(2,),
+        )
+        self.conv = ConvLayer(out_channels + out_channels, out_channels)
 
-    def forward(self):
-        pass
+    def forward(self, x, context):
+        x_out = self.conv_transpose(x)
+        x_out = torch.cat([x_out, context], dim=1)
+        x_out = self.conv(x_out)
+        return x_out
