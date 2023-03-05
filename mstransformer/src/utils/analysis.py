0ea36aedad3245a17e3f12a369dc1c9a5aa21195
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-03-04 16:35:01 -0800

    removed print statement in analysis.py

--- mstransformer/src/utils/analysis.py
+++ mstransformer/src/utils/analysis.py
@@ -1,6 +1,5 @@
 def get_number_of_parameters(model):
     num_param = 0
     for name, param in model.named_parameters():
-        print(name)
         num_param += param.numel()
     return num_param
